f85dbe7fce3075c11c7e7e21c2ee277fbd1c15c5
fachunka <kevanmurtagh@gmail.com>
2018-09-08 15:55:17 +0300

    Door animation

---
+++ Assets/DoorAnim.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnim : MonoBehaviour {
+    
+    public Animator ani;
+
+	// Use this for initialization
+	void Start () {
+        ani.enabled = false;
+	}
+	
+    void OnTriggerEnter (Collider col)
+    {
+        if(col.gameObject.name == "RightHand" || col.gameObject.name == "LeftHand")
+        {
+            ani.enabled = true;
+            Debug.Log("Hello");
+        }
+    }
+    
+    void OnTriggerExit (Collider col)
+    {
+        if(col.gameObject.name == "RightHand" || col.gameObject.name == "LeftHand")
+        {
+            ani.enabled = false;
+            Debug.Log("Goodbye");
+        }
+    }
+}
