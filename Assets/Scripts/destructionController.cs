c9d6616c73731e13cf7b5bf7c7c321e55d44c5a9
fachunka <kevanmurtagh@gmail.com>
2018-09-08 14:57:34 +0300

    Exploding cube (press space)

---
+++ Assets/Scripts/destructionController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destructionController : MonoBehaviour {
+    
+//    public GameObject remains;
+
+    [SerializeField]
+    private GameObject remains;
+    
+	// Use this for initialization
+	void Start () {
+	}
+    
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("space"))
+        {
+            Instantiate(remains, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+		
+	}
+}
