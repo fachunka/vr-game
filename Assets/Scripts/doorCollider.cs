a7dedc1d33ec8340fd4ae121c2be4a510310f682
fachunka <kevanmurtagh@gmail.com>
2018-09-08 14:13:37 +0300

    scene change and scissor physics

---
+++ Assets/Scripts/doorCollider.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class doorCollider : MonoBehaviour {
+
+    void OnTriggerEnter (Collider col)
+    {
+        if(col.gameObject.name == "Door")
+        {
+            SceneManager.LoadScene("SampleScene2");
+        }
+    }
+}
---
+++ Assets/Scripts/doorColliderTwo.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class doorColliderTwo : MonoBehaviour {
+
+    void OnTriggerEnter (Collider col)
+    {
+        if(col.gameObject.name == "Door")
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
