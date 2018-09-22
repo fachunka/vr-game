8a9b18a2575a9306378f4c7e29a2a4726898b868
murtagk1 <kevan.murtagh@aalto.fi>
2018-09-22 12:20:22 +0300

    Lift test

--- Assets/Scripts/doorCollider.cs
+++ Assets/Scripts/doorCollider.cs
@@ -4,12 +4,34 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class doorCollider : MonoBehaviour {
-
+    
+    public bool inLift = false;
+    public GameObject Lift;
+    
+    void Start()
+    {
+        Lift = GameObject.Find("Lift");
+    }
+    
+    void FixedUpdate()
+    {
+        if(inLift){
+        transform.parent.position -= transform.up * Time.deltaTime;
+        Lift.transform.position -= transform.up * Time.deltaTime;
+//        Debug.Log("hi", Lift);
+        }
+    }
+    
     void OnTriggerEnter (Collider col)
     {
         if(col.gameObject.name == "Door")
         {
             SceneManager.LoadScene("SampleScene2");
+        }
+        if(col.gameObject.name == "Lift")
+        {
+            inLift = true;
+            Debug.Log("you're now in a lift");
         }
     }
 }
