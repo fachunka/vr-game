4d4251cd67e109e6894658d8b57436c173b97034
Matias Harju <matias.harju@aalto.fi>
2018-11-18 15:53:04 +0200

    audio: meat screen beeps

--- Assets/Scripts/AudioScripts/meatScreenAudio.cs
+++ Assets/Scripts/AudioScripts/meatScreenAudio.cs
@@ -6,15 +6,34 @@ using UnityEngine.Audio;
 public class meatScreenAudio : MonoBehaviour
 {
 
-    public AudioClip[] screenClips;
+    public AudioClip beepPork;
+    public AudioClip beepChicken;
     public AudioSource screenSource;
+
+    private int beepNumberInt = 0;
+    private bool porkPlayed = false;
+    private bool chickenPlayed = false;
 
     public void playMeatScreen(float beepNumber)
     {
-        int beepNumberInt = (int)beepNumber;
-        screenSource.clip = screenClips[beepNumberInt];    // 0 = beep up, 1 = beep down
-        screenSource.Play();
-        print(beepNumber);
+        beepNumberInt = (int)beepNumber;
+
+        if (beepNumberInt == 1 && porkPlayed == false)
+        {
+            screenSource.clip = beepPork;
+            screenSource.Play();
+            porkPlayed = true;
+            chickenPlayed = false;
+        }
+
+        if (beepNumberInt == 2 && chickenPlayed == false)
+        {
+            screenSource.clip = beepChicken;
+            screenSource.Play();
+            chickenPlayed = true;
+            porkPlayed = false;
+        }
+
     }
 
 }
