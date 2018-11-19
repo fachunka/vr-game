8f6327e8b0157614cb682ded4ecebf767454cb91
Jung Huh <jhuh3226@gmail.com>
2018-11-19 17:38:16 +0200

    - (~ing) adding ugali dish

--- Assets/Scripts/OtherScripts/ReplaceUgali.cs
+++ Assets/Scripts/OtherScripts/ReplaceUgali.cs
@@ -2,16 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//if three 
-public class ReplaceUgali : MonoBehaviour {
+//if three items are placed over the stove, initiatate ugali dish
+public class ReplaceUgali : MonoBehaviour
+{
+    public GameObject Ugali;
+    bool replaceObjects;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        replaceObjects = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if colliding with corn & chicken & key?
+        if (other.gameObject.tag == "Key" && other.gameObject.tag == "Key" && other.gameObject.tag == "Key")
+        {
+            //and if button has been pressed
+            //if ()
+            //{
+                replaceObjects = true;
+                GameObject ugali = Instantiate(Ugali, transform.position, transform.rotation) as GameObject;
+            //}
+        }
+    }
 }
