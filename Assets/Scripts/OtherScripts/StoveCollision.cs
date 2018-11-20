ef5eb4373e21b9ccde944d92d17e9122a94a25e6
Jung Huh <jhuh3226@gmail.com>
2018-11-20 14:51:43 +0200

    -replace ugali dish ongoing

---
+++ Assets/Scripts/OtherScripts/StoveCollision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoveCollision : MonoBehaviour
+{
+    public bool ingredientsCollided;
+
+    // Use this for initialization
+    void Start()
+    {
+        ingredientsCollided = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Cornmeal" && collision.gameObject.tag == "Chicken" && collision.gameObject.tag == "MirendaLeaf")
+        {
+            ingredientsCollided = true;
+        }
+    }
+}
