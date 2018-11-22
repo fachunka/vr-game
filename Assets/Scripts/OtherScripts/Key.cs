6a9734ef36a6b665928c64bc796b4f04da965b46
Jung Huh <jhuh3226@gmail.com>
2018-11-22 15:28:34 +0200

    - error fixing2 after vr testing

--- Assets/Scripts/OtherScripts/Key.cs
+++ Assets/Scripts/OtherScripts/Key.cs
@@ -2,34 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Key : MonoBehaviour {
+public class Key : MonoBehaviour
+{
+    public GameObject chestHalf;
 
-	private bool keyInBox = false;
 
-	// Use this for initialization
-	void Start () {
+    private bool keyInBox = false;
 
-	}
+    // Use this for initialization
+    void Start()
+    {
 
-	// Update is called once per frame
-	void Update () {
+    }
 
-		if (keyInBox == true)
-		{
-				Destroy(this.gameObject);
-		}
+    // Update is called once per frame
+    void Update()
+    {
 
-	}
+        if (keyInBox == true)
+        {
+            Destroy(this.gameObject);
+        }
 
-	void OnTriggerEnter(Collider other)
-	{
+    }
 
-			if (other.gameObject.name == "boxClosedPrefab")
-			{
-					keyInBox = true;
-					print("collided key");
+    void OnTriggerEnter(Collider other)
+    {
 
-			}
+        if (other.gameObject.name == "boxClosedPrefab")
+        {
+            keyInBox = true;
+            print("collided key");
 
-	}
+            chestHalf = Instantiate(chestHalf, transform.position, Quaternion.identity) as GameObject;
+            Destroy(GameObject.Find("boxHalfOpenPrefab"));
+        }
+
+    }
 }
