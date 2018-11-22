7a8569fefd66a693c19f02426f5ac3f525f17243
Jung Huh <jhuh3226@gmail.com>
2018-11-22 14:04:53 +0200

    - change chest material - add code on chest open

---
+++ Assets/Scripts/OtherScripts/ChestFullyOpen.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestFullyOpen : MonoBehaviour
+{
+
+    public GameObject chestFully;
+
+    public bool chestFullyOpened;
+    // Use this for initialization
+    void Start()
+    {
+        chestFullyOpened = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Lime")
+        {
+            Debug.Log("chest fully opened");
+
+            chestFully = Instantiate(chestFully, transform.position, Quaternion.identity) as GameObject;
+            //destroy half opened chest
+            Destroy(gameObject);
+            //drop Lime from hand;
+
+            chestFullyOpened = true;
+        }
+    }
+}
---
+++ Assets/Scripts/OtherScripts/ChestOpen.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestOpen : MonoBehaviour
+{
+    public GameObject chestHalf;
+
+    public bool chestHalfOpened;
+    // Use this for initialization
+    void Start()
+    {
+        chestHalfOpened = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Key")
+        {
+            Debug.Log("chest half opened");
+
+            chestHalf = Instantiate(chestHalf, transform.position, Quaternion.identity) as GameObject;
+            //destroy closed chest
+            Destroy(gameObject);
+            //drop Key from hand;
+
+            chestHalfOpened = true;
+        }
+    }
+}
