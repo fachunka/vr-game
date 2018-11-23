a725aff681b85e0a5321791f94f2d98594b9033c
Matias Harju <matias.harju@aalto.fi>
2018-11-23 15:44:59 +0200

    Chest/Box clatter and opening sounds

---
+++ Assets/Scripts/AudioScripts/BoxClatterAudio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BoxClatterAudio : MonoBehaviour {
+
+    public AudioClip[] boxClatterClips;
+    private AudioSource audioSource;
+
+    void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == "Key" || other.gameObject.tag == "Lime" 
+            || other.gameObject.tag == "GameController" || other.gameObject.tag == "MainCamera"
+            || other.gameObject.tag == "Player")
+            {
+                // Don't play any sound
+            }
+
+            else
+            {
+                audioSource = gameObject.GetComponent<AudioSource>();
+                int randClatter = Random.Range (0, boxClatterClips.Length);
+                audioSource.clip = boxClatterClips[randClatter];
+                audioSource.Play();
+            }
+        }
+
+
+}
