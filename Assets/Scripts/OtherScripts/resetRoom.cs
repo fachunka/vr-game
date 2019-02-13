04de00a46da9edd68f7935665ef8531c90e24cd2
fachunka <kevanmurtagh@gmail.com>
2019-02-13 12:27:42 +0200

    reset room box prefab

---
+++ Assets/Scripts/OtherScripts/resetRoom.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resetRoom : MonoBehaviour {
+
+	// void Start() {
+	// 	PlayerPrefs.SetInt("Score", 20);
+	// 	PlayerPrefs.DeleteAll();
+	// }
+
+	// void Update() {
+	// 	Debug.Log(PlayerPrefs.GetInt("Score").ToString());
+	// }
+
+
+	void OnTriggerEnter(Collider other)
+	{
+			if (other.gameObject.tag == "GameController")
+			{
+ 			PlayerPrefs.DeleteAll();
+			}
+
+	}
+}
