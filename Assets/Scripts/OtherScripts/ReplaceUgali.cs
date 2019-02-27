af36dae89ccce88fa9fc903ed3a8673435d2fa71
fachunka <kevan.murtagh@aalto.fi>
2019-02-27 19:25:20 +0200

    ugali position

--- Assets/Scripts/OtherScripts/ReplaceUgali.cs
+++ Assets/Scripts/OtherScripts/ReplaceUgali.cs
@@ -21,7 +21,7 @@ public class ReplaceUgali : MonoBehaviour
     {
         replaceObjects = false;
         handNotTouchedButtonBefore = true;
-        UgaliPosition = new Vector3(0f, 0.11f, -0.8f);
+        UgaliPosition = new Vector3(0.1f, 0.3f, -0.5f);
 
         buttonPressed = false;
     }
