402faddebb4c8482706e1626badf3442b07700a3
Matias Harju <matias.harju@aalto.fi>
2019-05-08 11:57:40 +0300

    added welcome video audio (demo) to start scene

---
+++ Assets/Scripts/AudioScripts/StartStoryAudio.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class StartStoryAudio : MonoBehaviour
+{
+    public float waitBeforeVideoSound;   // wait before starting the voice over clip
+
+    public AudioSource videoAudioSource;    // where the video audio should emit from
+    public AudioClip videoAudioClip;    // audio clip for the video audio
+
+    //change scene
+    public string sceneName;
+    private float fadeDuration = 1f;
+    private SteamVR_TrackedObject trackedObj;
+
+    void Start()
+    {
+        StartCoroutine(playVideoSound());
+        PlayerPrefs.DeleteAll();
+    }
+
+
+    IEnumerator playVideoSound()
+    {
+        yield return new WaitForSeconds(waitBeforeVideoSound);
+        videoAudioSource.clip = videoAudioClip;
+        videoAudioSource.loop = false;
+        videoAudioSource.Play();
+        yield return new WaitForSeconds(0);
+    }
+
+
+    void Update()
+    {
+        if (videoAudioSource.time >= videoAudioSource.clip.length)
+        {
+            FadeToBlack();
+            SteamVR_LoadLevel.Begin(sceneName);
+        }
+    }
+
+    private void FadeToBlack()
+    {
+        //set start color
+        SteamVR_Fade.Start(Color.clear, 0f);
+        //set and start fade to
+        SteamVR_Fade.Start(Color.black, fadeDuration);
+
+    }
+
+
+}
