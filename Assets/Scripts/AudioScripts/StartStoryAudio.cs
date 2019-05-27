7ce9318561136012c81d24334a244eb792392c0e
kev <kevan.murtagh@aalto.fi>
2019-05-27 16:06:01 +0300

    start scene animations

--- Assets/Scripts/AudioScripts/StartStoryAudio.cs
+++ Assets/Scripts/AudioScripts/StartStoryAudio.cs
@@ -10,6 +10,21 @@ public class StartStoryAudio : MonoBehaviour
     public AudioSource videoAudioSource;    // where the video audio should emit from
     public AudioClip videoAudioClip;    // audio clip for the video audio
 
+    //Animation objects
+
+    public GameObject animation1;
+    
+
+    public GameObject animation2;
+    public float startTimeAnimation2;
+
+    public GameObject animation3;
+    public float startTimeAnimation3;
+    public GameObject animation4;
+    public float startTimeAnimation4;
+    public GameObject animation5;
+    public float startTimeAnimation5;
+
     //change scene
     public string sceneName;
     private float fadeDuration = 1f;
@@ -19,6 +34,9 @@ public class StartStoryAudio : MonoBehaviour
     {
         StartCoroutine(playVideoSound());
         PlayerPrefs.DeleteAll();
+        animation1.SetActive(true);
+        animation2.SetActive(false);
+        animation3.SetActive(false);
     }
 
 
@@ -34,6 +52,29 @@ public class StartStoryAudio : MonoBehaviour
 
     void Update()
     {
+        Debug.Log(videoAudioSource.time);
+
+        if (videoAudioSource.time >= startTimeAnimation2)
+        {
+            animation1.SetActive(false);
+            animation2.SetActive(true);
+        }
+
+        if (videoAudioSource.time >= startTimeAnimation3)
+        {
+            animation2.SetActive(false);
+            animation3.SetActive(true);
+        }
+        if (videoAudioSource.time >= startTimeAnimation4)
+        {
+            animation3.SetActive(false);
+            animation4.SetActive(true);
+        }
+        if (videoAudioSource.time >= startTimeAnimation5)
+        {
+            animation4.SetActive(false);
+            animation5.SetActive(true);
+        }
         if (videoAudioSource.time >= videoAudioSource.clip.length)
         {
             FadeToBlack();
