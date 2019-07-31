67c187e4c888103b23915e1cf9eede1c5a404110
unknown <matias.harju@iki.fi>
2019-07-31 12:25:00 +0300

    added music to The End Scene, sound levels not yet checked

---
+++ Assets/Scripts/AudioScripts/Music.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour {
+
+	public AudioClip musicStart;
+	public AudioClip musicLoop;
+
+	public float musicVolume;
+
+	private AudioSource audioSource;
+	void Start () {
+		audioSource = gameObject.GetComponent<AudioSource>();
+		audioSource.clip = musicStart;
+		audioSource.volume = musicVolume;
+		audioSource.loop = false;
+        audioSource.Play();
+	}
+	
+	void Update () {
+		if (!audioSource.isPlaying)
+		{
+			audioSource.clip = musicLoop;
+			audioSource.volume = musicVolume;
+			audioSource.loop = true;
+    	    audioSource.Play();
+		}
+
+	}
+}
